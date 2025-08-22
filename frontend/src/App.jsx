bb6355cbb56980b00c0b02c7ad7b40b1d71a6bb0
Suraj Kasaudhan <kasaudhansuraj25@gmail.com>
2025-08-22 16:16:58 +0530

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingScreen from './components/LandingScreen';
-import LoginScreen from './components/LoginScreen';
-import SignupScreen from './components/SignupScreen';
-import ProfileScreen from './components/ProfileScreen';
+import LandingScreen from './Components/LandingScreen.jsx';
+import LoginScreen from './Components/LoginScreen.jsx';
+import SignupScreen from './Components/SignupScreen.jsx';
+import ProfileScreen from './Components/ProfileScreen.jsx';
+// import LandingScreen from './components/LandingScreen';
+// import LoginScreen from './components/LoginScreen';
+// import SignupScreen from './components/SignupScreen';
+// import ProfileScreen from './components/ProfileScreen';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/signup" element={<SignupScreen />} />
-        <Route path="/profile" element={<ProfileScreen />} />
+        <Route path="/" element={<LandingScreen/>} />
+        <Route path="/login" element={<LoginScreen/>} />
+        <Route path="/signup" element={<SignupScreen/>} />
+        <Route path="/profile" element={<ProfileScreen/>} />
       </Routes>
     </Router>
   );
